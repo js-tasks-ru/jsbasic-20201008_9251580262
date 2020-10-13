6a0505746362d52f3d51ed665c0b40bf08980486
Anton Rokunets <9251580262@mail.ru>
2020-10-13 15:33:59 +0300

    lesson_01

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -4,5 +4,12 @@
  * @returns {boolean}
  */
 function checkSpam(str) {
-  // ваш код...
+  const spam1 = '1xBet'.toUpperCase(),
+        spam2 = 'XXX'.toUpperCase();
+        str = str.toUpperCase();
+  if(str.includes(spam1) || str.includes(spam2)){
+    return true
+  }
+  return false;  
 }
+
