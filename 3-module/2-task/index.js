ce79dd94382620bf1b9d1902f6b1e3bbcf83f31a
Anton Rokunets <9251580262@mail.ru>
2020-10-27 18:45:05 +0300

    правка

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -5,10 +5,9 @@
  * @returns {number[]}
  */
 
-let arr = [5, 3, 8, 1];
 
 function filterRange(arr, a, b) {
   return arr.filter(item => (a <= item && item < b))
 }
-let filtered = filterRange(arr, 1, 4);
+
 
