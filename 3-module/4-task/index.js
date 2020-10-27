2b279a7db35254dfef50d55ff72015a6e20b3c8a
Anton Rokunets <9251580262@mail.ru>
2020-10-27 15:30:11 +0300

    lesson_03

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -6,37 +6,14 @@
  */
 
 
-let user1 = {
-  "balance": "$1,825.65",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Golden Branch",
-  "gender": "male",
-  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let user2 = {
-  "balance": "$1,490.15",
-  "picture": "https://placehold.it/32x32",
-  "age": 21,
-  "name": "Duncan Randall",
-  "gender": "male",
-  "greeting": "Hello, Duncan Randall! You have 7 unread messages.",
-  "favouriteFruit": "banana"
-};
-
-let users = [user1, user2];
-
 function showSalary(users, age) {
   let result = '';
   users.forEach(item => {
-    if(age >= item.age){
+    if (age >= item.age) {
       result += `${item.name}, ${item.balance}\n`
     }
-  }); 
+  });
   result = result.slice(0, result.length - 1);
   return result;
 }
 
-let result = showSalary(users, 40);
