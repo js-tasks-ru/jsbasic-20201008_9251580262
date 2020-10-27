73793f4860940aa2638b4119bb2ccf093b1b8485
Anton Rokunets <9251580262@mail.ru>
2020-10-27 22:09:03 +0300

    lesson04_1

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -7,7 +7,7 @@ function makeFriendsList(friends) {
   const ul = document.createElement('ul');
   document.body.insertAdjacentElement('afterbegin', ul);
   friends.forEach(item => {
-    ul.insertAdjacentHTML('beforebegin', `<li>${item.firstName} ${item.lastName}</li>`);
+    ul.insertAdjacentHTML('beforeend', `<li>${item.firstName} ${item.lastName}</li>`);
   });
   return ul;
 }
