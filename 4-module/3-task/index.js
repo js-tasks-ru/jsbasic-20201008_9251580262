d7b57f36267617f0dba20bf943f85ef77b363a6f
Anton Rokunets <9251580262@mail.ru>
2020-10-27 23:30:49 +0300

    lesson04_3

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,24 @@
  * @param {Element} table
  */
 function highlight(table) {
+  for (let i = 0; i < table.rows.length; i++) {
+    const columns = [...table.rows[i].cells];
+
+    columns.forEach(item => {
+      if (item.dataset.available === 'true') {
+        item.parentElement.classList.add('available');
+      } else if (item.dataset.available === 'false') {
+        console.dir(item);
+        item.parentElement.classList.add('unavailable');
+      } else if (item.cellIndex === 3 && item.dataset.available === undefined && item.textContent !== 'Status') {
+        item.parentElement.setAttribute('hidden', '');
+      } else if (item.cellIndex === 2 && item.textContent === 'm') {
+        item.parentElement.classList.add('male');
+      } else if (item.cellIndex === 2 && item.textContent === 'f') {
+        item.parentElement.classList.add('female');
+      } else if (item.cellIndex === 1 && Number(item.textContent) < 18) {
+        item.parentElement.style.textDecoration = 'line-through';
+      }
+    });
+  }
 }
