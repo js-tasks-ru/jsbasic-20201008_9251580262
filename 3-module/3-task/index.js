2d9db507247356c00dd41cc2a5a7a1a1684083ef
Anton Rokunets <9251580262@mail.ru>
2020-10-27 18:46:27 +0300

    правка

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -4,13 +4,10 @@
  */
 function camelize(str) {
   str = str.trim().split('-');
-  str = str.map( item =>{
+  str = str.map(item => {
     (item === str[0]) ? item : item = item[0].toUpperCase() + item.slice(1);
     return item;
   })
   return str.join('');
 }
 
-camelize('background-color');
-camelize('list-style-image');
-camelize('-webkit-transition');
