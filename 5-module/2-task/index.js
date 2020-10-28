9c29a90d2d5ca85ac674830d8f87931d780c6250
Anton Rokunets <9251580262@mail.ru>
2020-10-28 09:49:40 +0300

    lesson05_2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  const text = document.getElementById('text');
+  const toggleTextButton = document.querySelector('.toggle-text-button');
+  toggleTextButton.onclick = () => {
+    text.attributes[1] ? text.removeAttribute('hidden') : text.setAttribute('hidden', '');
+  }
 }
