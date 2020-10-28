77350a2098c6be26dbb2d0747f3bf3f8ab534b81
Anton Rokunets <9251580262@mail.ru>
2020-10-28 15:53:05 +0300

    lesson05-3

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  const carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  const carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  const carouselSlide = [...document.querySelectorAll('.carousel__slide')];
+
+  carouselArrowLeft.style.display = 'none';
+  document.body.addEventListener('click', (event) => {
+    const target = event.target;
+    let shift = carouselSlide[0].offsetWidth;
+    if (target === carouselArrowRight) {
+      carouselArrowLeft.style.display = '';
+
+      if (carouselSlide[0].style.transform === 'translateX(0px)' || carouselSlide[0].style.transform === '') {
+        shift = carouselSlide[0].offsetWidth;
+      } else if (carouselSlide[0].style.transform !== '') {
+        shift += parseInt(carouselSlide[0].style.transform.slice(12, 17));
+      }
+
+      carouselSlide.forEach(item => {
+        if (shift + carouselSlide[0].offsetWidth === carouselSlide[0].offsetWidth * carouselSlide.length) {
+          carouselArrowRight.style.display = 'none'
+        }
+        item.style.transform = `translateX(-${shift}px)`;
+      })
+
+    } else if (target === carouselArrowLeft) {
+      if (carouselSlide[0].style.transform !== '' || carouselSlide[0].style.transform !== 'translateX(0px)') {
+        shift = parseInt(carouselSlide[0].style.transform.slice(12, 17)) - carouselSlide[0].offsetWidth;
+        carouselArrowRight.style.display = ''
+      }
+      carouselSlide.forEach(item => {
+        if (shift === 0) {
+          carouselArrowLeft.style.display = 'none'
+        }
+        item.style.transform = `translateX(-${shift}px)`;
+      })
+    }
+  })
 }
